3150027a7cafcc38c7089d4e237ad472c584f205
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-03-12 11:05:13 +0000

    deploy: 29c7d030228ec72155e436880ab9836d3b44790a

---
+++ assets/Contact-D4IcKFEe.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-DQ_yGtv9.js";function o(){return t.jsx("div",{children:"Contact"})}export{o as default};
--- assets/Features-nASOO_a9.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-DhuvOmyf.js";function e(){return r.jsx("div",{children:"Fratures"})}export{e as default};
