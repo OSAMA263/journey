439690c6797e6bb3ca9e18ce4044d01b996f2e9f
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-03-13 11:28:36 +0000

    deploy: 5acc7ea865fbe0bbfba9bd008723aaf08db20d4e

--- assets/Contact-D4IcKFEe.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-DQ_yGtv9.js";function o(){return t.jsx("div",{children:"Contact"})}export{o as default};
--- assets/Features-C_LuPuD0.js
+++
@@ -1,7 +0,0 @@
-import{j as s,L as d,G as c,S as r,a as l,T as i,p as t,d as a,f as m,b as g,C as n,F as x}from"./index-DQ_yGtv9.js";import{P as j}from"./PricingSection-C9kQqP7X.js";function h(){return s.jsxs(d,{children:[s.jsx(c,{glowing:"!-top-0",className:"py-40 text-center flex flex-col",children:s.jsx(r,{smallTitle:"Features",largeTitle:"Boost your Productivity & drive more Sales"})}),s.jsxs(l,{className:"grid-cols-[1.25fr_1fr]",children:[s.jsx(r,{smallTitle:"Productivity",largeTitle:"AI Driven Statistics"}),s.jsx(i,{br:!0,smallImg:t,bigImg:a})]}),s.jsx(u,{children:m.map((o,e)=>s.jsx(g,{className:e===0?"row-span-2":e===3?"col-span-2":"",children:s.jsx(n,{border:!0,data:o,children:e===0&&s.jsxs("div",{className:"relative",children:[s.jsx("div",{className:"absolute left-0 top-0 w-full h-full bg-gradient-to-t from-black-color"}),s.jsx("img",{src:a,alt:"dashboard"})]})})},e))}),s.jsxs(l,{className:"grid-cols-[1fr_1.25fr]",children:[s.jsx(i,{br:!0,smallImg:t,bigImg:a}),s.jsx(r,{smallTitle:"Analytics",largeTitle:"Detailed Statistics"})]}),s.jsx(j,{})]})}const u=x.div`
-grid
-grid-cols-3
-grid-rows-2
-gap-10
-my-32
-`;export{h as default};
