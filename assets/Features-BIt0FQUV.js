f1a586d2c8cfe1cfc2d58b30b77790a851f8d1e1
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-03-14 11:31:06 +0000

    deploy: 5c368d44d80ffda6fc3c1ac35c21bf0f90baa322

--- assets/Features-BIt0FQUV.js
+++
@@ -1,7 +0,0 @@
-import{j as s,L as c,G as d,a,S as l,h as i,i as t,k as r,l as g,c as m,C as n,P as x,e as j}from"./index-BwhEYoDR.js";function h(){return s.jsxs(c,{children:[s.jsx(d,{glowing:"!-top-0",className:"pt-40 text-center flex flex-col",children:s.jsx(a,{smallTitle:"Features",largeTitle:"Boost your Productivity & drive more Sales"})}),s.jsxs(l,{className:"grid-cols-[1.25fr_1fr]",children:[s.jsx(a,{smallTitle:"Productivity",largeTitle:"AI Driven Statistics"}),s.jsx(i,{br:!0,smallImg:t,bigImg:r})]}),s.jsx(u,{children:g.map((o,e)=>s.jsx(m,{className:e===0?"row-span-2":e===3?"col-span-2":"",children:s.jsx(n,{border:!0,data:o,children:e===0&&s.jsxs("div",{className:"relative",children:[s.jsx("div",{className:"absolute left-0 top-0 w-full h-full bg-gradient-to-t from-black-color"}),s.jsx("img",{src:r,alt:"dashboard"})]})})},e))}),s.jsxs(l,{className:"grid-cols-[1fr_1.25fr]",children:[s.jsx(i,{br:!0,smallImg:t,bigImg:r}),s.jsx(a,{smallTitle:"Analytics",largeTitle:"Detailed Statistics"})]}),s.jsx(x,{})]})}const u=j.div`
-grid
-grid-cols-3
-grid-rows-2
-gap-10
-my-32
-`;export{h as default};
---
+++ assets/Features-ClRgsctQ.js
@@ -0,0 +1,7 @@
+import{j as s,L as d,G as c,d as a,S as l,m as t,n as i,o as r,q as m,e as g,C as n,P as x,T as j,h as u,f,g as h}from"./index-DPYzTLAc.js";function S(){return s.jsxs(d,{children:[s.jsx(c,{glowing:"!-top-0",className:"pt-40 text-center flex flex-col",children:s.jsx(a,{smallTitle:"Features",largeTitle:"Boost your Productivity & drive more Sales"})}),s.jsxs(l,{className:"grid-cols-[1.25fr_1fr]",children:[s.jsx(a,{smallTitle:"Productivity",largeTitle:"AI Driven Statistics"}),s.jsx(t,{br:!0,smallImg:i,bigImg:r})]}),s.jsx(p,{children:m.map((o,e)=>s.jsx(g,{className:e===0?"row-span-2":e===3?"col-span-2":"",children:s.jsx(n,{border:!0,data:o,children:e===0&&s.jsxs("div",{className:"relative",children:[s.jsx("div",{className:"absolute left-0 top-0 w-full h-full bg-gradient-to-t from-black-color"}),s.jsx("img",{src:r,alt:"dashboard"})]})})},e))}),s.jsxs(l,{className:"grid-cols-[1fr_1.25fr]",children:[s.jsx(t,{br:!0,smallImg:i,bigImg:r}),s.jsx(a,{smallTitle:"Analytics",largeTitle:"Detailed Statistics"})]}),s.jsx(x,{}),s.jsx(j,{}),s.jsx(u,{}),s.jsx(f,{})]})}const p=h.div`
+grid
+grid-cols-3
+grid-rows-2
+gap-10
+my-32
+`;export{S as default};
