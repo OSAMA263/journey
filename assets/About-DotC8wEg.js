ca8ec877f2df23843210d699be5f055dd610117b
OSAMA263 <OSAMA263@users.noreply.github.com>
2024-03-17 10:11:47 +0000

    deploy: bb952660cae814aea3504198c679a31d1f5c2a2f

---
+++ assets/About-DotC8wEg.js
@@ -0,0 +1,24 @@
+import{j as e,F as t,a as n,b as l,c,L as j,G as x,S as y,d,A as b,e as a,T as w,f as v,M as f,g as m}from"./index-DA59Q5Xg.js";const N=[{img:"employe2.jpg",name:"Alex Thompson",position:"Software Enginer",description:"Meet Alex, our dedicated software engineer who turns complex problems into elegant solutions.",socials:[{logo:e.jsx(t,{})},{logo:e.jsx(n,{})},{logo:e.jsx(l,{})},{logo:e.jsx(c,{})}]},{img:"employe3.jpg",name:"Jordan Rodriguez",position:"Markteing Lead",description:"Jordan, our marketing maestro, is the driving force behind our brand's visibility and success.",socials:[{logo:e.jsx(t,{})},{logo:e.jsx(n,{})},{logo:e.jsx(l,{})},{logo:e.jsx(c,{})}]},{img:"employe1.jpg",name:"MF DOOM",position:"A human",description:"He eats, drinks, sleeps and sleeps more.",socials:[{logo:e.jsx(t,{})},{logo:e.jsx(n,{})},{logo:e.jsx(l,{})},{logo:e.jsx(c,{})}]}],A=[{role:"Customer Support Specialist",location:"Anywhere",description:"Join our customer-focused team as a Customer Support Specialist, where your dedication will ensure our clients receive exceptional service."},{role:"Data Analyst",location:"Anywhere",description:"Join our team as a Data Analyst and turn raw data into actionable insights that drive informed business decisions."},{role:"UX/UI Designer",location:"Anywhere",description:"Shape user experiences and bring creative visions to life as a UX/UI Designer in our collaborative and innovative team."},{role:"Content Marketing Manager",location:"Anywhere",description:"Join our team as a Content Marketing Manager and be the driving force behind creating compelling narratives that resonate with our audience."},{role:"Software Engineer",location:"Anywhere",description:"We're seeking a skilled Software Engineer to join our innovative team, where your coding expertise will shape the future of our digital solutions."},{role:"Digital Marketing Specialist",location:"Anywhere",description:"Join our dynamic team as a Digital Marketing Specialist, where your creativity and strategic mindset will drive impactful online campaigns."}];function F(){return e.jsxs(j,{children:[e.jsx(x,{glowing:"!-top-1/2 !blur-[200px] !bg-opacity-75 ",className:"space-y-20",children:e.jsxs(y,{className:"grid-cols-[1.25fr_1fr] pt-40",children:[e.jsx(d,{smallTitle:"About Us",largeTitle:"A Company you can place your Trust in"}),e.jsx("img",{src:"/about hero.webp",alt:"hero img",className:"rounded-xl"})]})}),e.jsx(b,{}),e.jsxs(a,{className:"space-y-20 bg-dots text-balance text-center !mt-32",children:[e.jsx("h1",{children:"We create automated Processes that enhance Global Companies"}),e.jsx("div",{className:"grid md:grid-cols-4 text-start gap-8",children:Array.from({length:4}).map((o,s)=>e.jsxs(a,{i:s,className:"p-8 rounded-2xl border border-light-gray space-y-4",children:[e.jsx("h2",{className:"text-xl",children:"6M"}),e.jsx("p",{children:"Customers Genrated"})]},s))})]}),e.jsxs(x,{glowing:"!bg-opacity-25",className:"space-y-20",children:[e.jsx(d,{smallTitle:"Our Team",largeTitle:"Meet the Team",className:"text-center mx-auto"}),e.jsx("div",{className:"grid md:grid-cols-3 gap-20",children:N.map(({img:o,name:s,position:i,description:r,socials:g},p)=>e.jsxs("div",{className:"space-y-6 text-center",children:[e.jsx(a,{className:"rounded-2xl overflow-hidden w-[90%] max-md:w-1/2 mx-auto",children:e.jsx("img",{src:o,className:"hover:scale-110 w-full",alt:s})}),e.jsxs(a,{children:[e.jsx("h6",{children:s}),e.jsx("span",{children:i})]}),e.jsx(a,{children:e.jsx("p",{children:r})}),e.jsx(a,{className:"flex justify-center gap-x-4",children:g.map(({logo:u},h)=>e.jsx(M,{children:u},h))})]},p))})]}),e.jsx(w,{}),e.jsx("div",{children:e.jsx(d,{smallTitle:"We're Hiring",largeTitle:"Join our Team",className:"text-center mx-auto"})}),e.jsx(S,{}),e.jsx(v,{})]})}const S=()=>e.jsx(x,{glowing:"!bg-opacity-15",className:"space-y-8 !mt-20 sm:w-[69%] mx-auto",children:A.map(({role:o,location:s,description:i},r)=>e.jsx(a,{children:e.jsxs(T,{children:[e.jsxs("div",{className:"flex justify-between",children:[e.jsxs("div",{children:[e.jsx("h2",{className:"text-xl text-yellow-color",children:o}),e.jsx("span",{children:s})]}),e.jsx(f,{children:"Apply Now"})]}),e.jsx("p",{children:i})]})},r))}),M=m.small`
+border
+rounded-full
+transition-all
+duration-300
+p-2
+text-black
+cursor-pointer
+border-yellow-color
+bg-yellow-color
+hover:bg-transparent
+hover:text-yellow-color
+hover:scale-110
+`,T=m.div`
+space-y-8
+border
+cursor-pointer
+rounded-2xl
+border-light-gray
+hover:border-yellow-color
+p-10
+transition-all
+duration-300
+`;export{F as default};
---
+++ assets/Features-BAvw89BT.js
@@ -0,0 +1,9 @@
+import{j as s,L as d,G as c,d as e,S as r,m as t,n as i,o as l,q as g,e as m,C as n,P as x,T as j,h as u,f,g as h}from"./index-DA59Q5Xg.js";function S(){return s.jsxs(d,{children:[s.jsx(c,{glowing:"!-top-0",className:"pt-40 text-center flex flex-col",children:s.jsx(e,{smallTitle:"Features",largeTitle:"Boost your Productivity & drive more Sales"})}),s.jsxs(r,{className:"grid-cols-[1.25fr_1fr]",children:[s.jsx(e,{smallTitle:"Productivity",largeTitle:"AI Driven Statistics",className:"max-lg:order-1"}),s.jsx(t,{br:!0,smallImg:i,bigImg:l})]}),s.jsx(p,{children:g.map((o,a)=>s.jsx(m,{className:a===0?"lg:row-span-2 max-lg:col-span-2":a===3?"col-span-2":"",children:s.jsx(n,{border:!0,data:o,children:a===0&&s.jsxs("div",{className:"relative",children:[s.jsx("div",{className:"absolute left-0 top-0 w-full h-full bg-gradient-to-t from-black-color"}),s.jsx("img",{src:l,alt:"dashboard"})]})})},a))}),s.jsxs(r,{className:"grid-cols-[1fr_1.25fr]",children:[s.jsx(t,{br:!0,smallImg:i,bigImg:l}),s.jsx(e,{smallTitle:"Analytics",largeTitle:"Detailed Statistics"})]}),s.jsx(x,{}),s.jsx(j,{}),s.jsx(u,{}),s.jsx(f,{})]})}const p=h.div`
+grid
+lg:grid-cols-3
+grid-cols-2
+lg:grid-rows-2
+lg:gap-10
+gap-2
+my-32
+`;export{S as default};
